18cf1370d76be1d077598e6b701c777423244c4a
egy2025 <148514280+egy2025@users.noreply.github.com>
2023-10-19 21:30:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-from langchain.schema.embeddings import Embeddings
 
 
 def get_pdf_text(pdf_docs):
