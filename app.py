7bc00767499a4b0898fdae3bef0bb636792cece5
egy2025 <148514280+egy2025@users.noreply.github.com>
2023-10-19 21:53:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,17 +43,15 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    #llm = ChatOpenAI()
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    try:
+        llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+        memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+        conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
+        return conversation_chain
+    except Exception as e:
+        st.write(f"Error initializing conversation chain: {str(e)}")
+        return None
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
 
 
 def handle_userinput(user_question):
