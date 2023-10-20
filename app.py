b0c1484e73547e7eed5f5b221839cb08b71e2f70
egy2025 <148514280+egy2025@users.noreply.github.com>
2023-10-19 21:29:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from langchain.pydantic_v1 import BaseModel, Extra, Field
+from langchain.schema.embeddings import Embeddings
 
 
 def get_pdf_text(pdf_docs):
